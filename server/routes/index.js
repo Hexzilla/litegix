ea55f1b073e4fe2a42cb88307635fcc3b4fcdbf0
rightvalue <bluebird6922@gmail.com>
2021-06-22 06:56:44 +0300

    update packages

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,23 @@ const authService = require('../services/auth-service')
 router.use('/api', require('./api'));
 router.use('/settings', require('./settings'));
 router.use('/servers', require('./servers'));
+router.use('/installation', require('./installation'));
 
 router.post('/login', authService.login);
 router.post('/signup', authService.signup);
 
+router.use(function (err, req, res, next) {
+  if (err.name === "ValidationError") {
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function (errors, key) {
+        errors[key] = err.errors[key].message
+
+        return errors
+      }, {}),
+    })
+  }
+
+  return next(err)
+})
 
 module.exports = router;
--- server/routes/settings/index.js
+++ server/routes/settings/index.js
@@ -4,18 +4,4 @@ router.use("/profile", require("./profile"))
 router.use("/account", require("./account"))
 router.use("/apikey", require("./apikey"))
 
-router.use(function (err, req, res, next) {
-  if (err.name === "ValidationError") {
-    return res.status(422).json({
-      errors: Object.keys(err.errors).reduce(function (errors, key) {
-        errors[key] = err.errors[key].message
-
-        return errors
-      }, {}),
-    })
-  }
-
-  return next(err)
-})
-
 module.exports = router
