24a3787da7b7f612e87c63577e10b514bc5c53c9
rightvalue <bluebird6922@gmail.com>
2021-07-03 04:58:43 +0300

    change password

--- server/services/auth-service.js
+++ server/services/auth-service.js
@@ -17,9 +17,7 @@ const getUser = async function (req) {
 
     const user = await User.findById(req.payload.id)
     if (!user) {
-      return reject({
-        message: "User isn't exists" 
-      })
+      return reject({ message: "User isn't exists" })
     }
     return { user: user }
   }
@@ -62,7 +60,30 @@ const signup = function (req, res, next) {
 }
 
 const changePassword = async function (req, res) {
+  try {
+    let {user, errors} = await getUser(req)
+    if (errors) {
+      return res.status(422).json({ success: false, errors: errors })
+    }
 
+    if (!user.validPassword(req.body.current_password)) {
+      return res.status(422).json({ success: false, errors: { "current_password": "is invalid" }})
+    }
+
+    user.setPassword(req.body.password)
+    await user.save()
+
+    return res.json({
+      success: true,
+      message: "Your password has been successfully changed"
+    })
+  }
+  catch (error) {
+    return res.status(501).json({
+      success: false,
+      errors: error
+    });
+  }
 }
 
 const deleteAccount = async function (req, res) {
