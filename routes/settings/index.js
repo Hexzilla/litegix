13261bdc84d2a73e5f7690a0eb34c2404bd023fd
rightvalue <bluebird6922@gmail.com>
2021-07-30 10:39:40 +0300

    remove externalapi

--- routes/settings/index.js
+++ routes/settings/index.js
@@ -3,7 +3,6 @@ var router = require("express").Router()
 router.use("/profile", require("./profile"))
 router.use("/account", require("./account"))
 router.use("/apikey", require("./apikey"))
-router.use("/externalapi", require("./externalapi"))
 router.use("/notifications", require("./notifications"))
 router.use("/ipwhitelist", require("./ipwhitelist"))
 
