2aeb3013229f1ca101e9736d5d3101c13a4b3ff3
goldencat-tom <bluebird6922@gmail.com>
2021-09-13 10:16:59 +0300

    update servers

--- routes/servers/servers.js
+++ routes/servers/servers.js
@@ -1,20 +1,21 @@
-const { body } = require('express-validator')
-const router = require("express").Router()
-const auth = require("../auth")
-const validate = require("../validate")
-const server = require("../../services/server")
+const { body } = require("express-validator");
+const router = require("express").Router();
+const auth = require("../auth");
+const validate = require("../validate");
+const server = require("../../services/server");
 
-router.get("/", auth.required, server.getServers)
+router.get("/", auth.required, server.getServers);
 
-router.post("/store", 
-  auth.required, 
-  body('name').notEmpty(),
-  body('address').isIP(4),
-  body('provider').notEmpty(),
-  body('web_server').notEmpty(),
-  body('database').notEmpty(),
-  body('phpVersion').notEmpty(),
+router.post(
+  "/store",
+  auth.required,
+  body("name").notEmpty(),
+  body("address").isIP(4),
+  body("web_server").notEmpty(),
+  body("database").notEmpty(),
+  body("phpVersion").notEmpty(),
   validate,
-  server.storeServer)
+  server.storeServer
+);
 
-module.exports = router
+module.exports = router;
