74f17625c93aa35af44b80a6feba55cf841a409f
goldencat-tom <bluebird6922@gmail.com>
2022-01-09 13:49:26 +0900

    update api/installation

--- src/routes/api/installation.ts
+++ src/routes/api/installation.ts
@@ -1,27 +1,19 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express'
+import {
+  createHandler as ch,
+  createHandlerAndSendText as chs,
+} from 'routes/helper'
 import * as install from 'services/install.service'
 
 const router = Router()
-router.get('/script/:token', async function (req: Request, res: Response) {
-  try {
-    const token = req.params.token
-    const text = await install.getAgentInstallScript(token)
-    res.send(text)
-  } catch (e) {
-    console.error(e)
-    return res.status(501).json({ success: false })
-  }
-})
+router.get(
+  '/script/:token',
+  chs(({ params }) => install.getAgentInstallScript(params.token))
+)
 
-router.post('/status/:token', async function (req: Request, res: Response) {
-  try {
-    const token = req.params.token
-    const result = await install.updateInstallState(token, req.body)
-    res.json(result)
-  } catch (e) {
-    console.error(e)
-    return res.status(501).json({ success: false })
-  }
-})
+router.post(
+  '/status/:token',
+  ch(({ body, params }) => install.updateInstallState(params.token, body))
+)
 
 export default router
--- src/routes/helper.ts
+++ src/routes/helper.ts
@@ -30,3 +30,16 @@ export function createHandler(callback: (req: Request) => object) {
     }
   }
 }
+
+export function createHandlerAndSendText(callback: (req: Request) => object) {
+  return async function (req: Request, res: Response) {
+    try {
+      return res.send(await callback(req))
+    } catch (e) {
+      return res.status(501).json({
+        success: false,
+        errors: errorMessage(e),
+      })
+    }
+  }
+}
